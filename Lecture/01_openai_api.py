875c811d52ba696c10872ebbce5c4d282d4195fd
shin0624 <qjatn147852@naver.com>
2024-03-12 12:43:58 +0900

    some init msg

---
+++ Lecture/01_openai_api.py
@@ -0,0 +1,28 @@
+# OPEN ai API사용하기(platform.openai.com)
+# 1. API 키 발급
+# 2. 카드등록
+
+#라이브러리관리
+# 1. venv
+# 2. anaconda
+#개발환경구축
+#python -m venv .\venv
+# .\venv\Scripts\activate
+#pip install openai
+from openai import OpenAI
+client = OpenAI(api_key = "")
+
+completion = client.chat.completions.create(
+  model="gpt-3.5-turbo",
+  messages=[
+    {"role": "system", "content":""},
+    {"role": "user", "content": "클라우드 설명해줘"}
+  ]
+)
+
+print(completion.choices[0].message)
+git init
+git config user.name "shin0624"
+git config user.email "qjatn147852@naver.com"
+git add *
+git commit -m "some init msg"
