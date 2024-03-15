a075f2e965d9b545d838215122446863992867e7
shin0624 <qjatn147852@naver.com>
2024-03-15 10:49:00 +0900

    Add _LangChain

--- Lecture/01_openai_api.py
+++ Lecture/01_openai_api.py
@@ -1,23 +1,18 @@
-# OPEN ai API사용하기(platform.openai.com)
-# 1. API 키 발급
-# 2. 카드등록
-
-#라이브러리관리
-# 1. venv
-# 2. anaconda
-#개발환경구축
-#python -m venv .\venv
-# .\venv\Scripts\activate
-#pip install openai
 from openai import OpenAI
 client = OpenAI(api_key = "")
 
 completion = client.chat.completions.create(
   model="gpt-3.5-turbo",
   messages=[
-    {"role": "system", "content":""},
+    {"role": "system", "content":"모든 설명을 3줄로 요약해서 설명해주세요."},
     {"role": "user", "content": "클라우드 설명해줘"}
   ]
 )
-
+print("\n")
 print(completion.choices[0].message)
+print("\n")
+#openAI의 API를 사용해서 챗봇 문제점
+# 1. 개발이 어려움(난이도 상)-->더 쉽게 개발할 수 있는 "?"(프레임워크) 필요
+# 2. 챗봇 개발 완성 -->Bard모델 변경 시 -->Bard API 처음부터 개발해야함 --> 프레임워크(LLM) 사용
+
+# --> LangChain 프레임워크(코드는 동일, 모델 바꾸면 자동으로 바꿔짐) --> pip install [라이브러리명]
--- README.md
+++ README.md
@@ -1,2 +1,23 @@
 # Chosun_Edu_ChatGPT
 OpenAI사의 GPT모델을 활용한 챗봇 개발하기
+#VS CODE를 사용한 파이썬 개발 환경 구축
+    1. VSCODE설치
+    2.파이썬 설치(python.org) + 환경변수(path)체크!
+    3. vscode에서 extentions(python, python Extension pack, gitgraph ofr gitlens) 추가
+    4. vscode에서 github 계정으로 로그인(연동)
+    5. github에서 repository(프로젝트) 생성
+    6. vscode에서 "git clone repository"로 5번에서 생성한 프로젝트 내려받기
+    7. 가상환경 생성(venv) : 가상환경 구축 참조
+    8. vscode에서 select python Interpreter 설정(ctrl+shift+P)->venv
+
+# OPEN ai API사용하기(platform.openai.com)
+# 1. API 키 발급
+# 2. 카드등록
+
+#라이브러리관리
+# 1. venv
+# 2. anaconda
+#개발환경구축
+#python -m venv .\venv
+# .\venv\Scripts\activate
+#pip install openai
